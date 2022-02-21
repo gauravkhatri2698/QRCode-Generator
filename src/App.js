02a2fdf9ffe99ac63a84499a01e1c6657e21369f
Gaurav Khatri <gaurav.khatri@polestarllp.com>
2022-02-21 12:38:47 +0530

    Added qr code generator app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
 import './App.css';
 
 function App() {
+
+  const [inputText, setInputText] = useState('');
+  const [qrCodeText, setQRCodeText] = useState('');
+
+  // generate QR code
+  const generateQRCode = () => {
+    setQRCodeText(inputText);
+  }
+
+  // download QR code
+  const downloadQRCode = () => {
+    const qrCodeURL = document.getElementById('qrCodeEl')
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    console.log(qrCodeURL)
+    let aEl = document.createElement("a");
+    aEl.href = qrCodeURL;
+    aEl.download = "QR_Code.png";
+    document.body.appendChild(aEl);
+    aEl.click();
+    document.body.removeChild(aEl);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Generate and download a QR code image in React</h3>
+      <div className="qr-input">
+        <input
+          type="text"
+          placeholder="Enter input"
+          value={inputText}
+          onChange={e => setInputText(e.target.value)}
+        />
+        <input
+          type="button"
+          value="Generate"
+          onClick={generateQRCode}
+        />
+      </div>
+      <div className='qrcode-generator'>
+        <QRCode
+          id="qrCodeEl"
+          size={200}
+          value={qrCodeText}
+        />
+      </div>
+      <br />
+      <div className='download-btn'>
+        <input
+          type="button"
+          value="Download"
+          onClick={downloadQRCode}
+        />
+      </div>
     </div>
   );
 }
